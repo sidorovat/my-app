f9c1268b0b952acc09b489cf8dc3845dec1ba501
Тетяна <sidorovatana957@stud.op.edu.ua>
2025-06-08 18:10:10 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>Мій сайт</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            padding: 50px;
+            background-color: #f2f2f2;
+            text-align: center;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #555;
+        }
+    </style>
+</head>
+<body>
+    <h1>Привіт!</h1>
+    <p>Це мій перший PHP-сайт, який я залила на хостинг через GitHub і Railway.</p>
+    <p>Все працює чудово</p>
+</body>
+</html>
